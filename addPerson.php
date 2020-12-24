c3a03e7945dd3a163acc6fe278c9d37f05682431
futuredev <futuredev1001@yandex.com>
2020-12-24 08:08:39 -0300

    Add person logic - my sql database connect

---
+++ addPerson.php
@@ -0,0 +1,41 @@
+<?php
+$firstname = $_POST['firstname'];
+$middlename = $_POST['middlename'];
+$lastname = $_POST['lastname'];
+$birthday = $_POST['birthday'];
+$gender = $_POST['gender'];
+$file = $_POST['file'];
+
+$servername = "database-1-instance-1.cwsml3emw73t.us-east-2.rds.amazonaws.com";
+$db_username = 'admin';
+$db_password = 'bit7wise';
+
+/*local database test */
+//$servername = "localhost";
+//$db_username = 'root';
+//$db_password = '';
+
+$databasename = "memoreebook";
+$conn = new mysqli($servername, $db_username, $db_password, $databasename);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$value = "('".$firstname."', '".$middlename."', '".$lastname."', '".$file."', '".$birthday."', ".$gender.")";
+
+$sql = "insert into family_user (firstName, middleName, lastName, profileImage, birthday, gender) values ".$value;
+
+if ($conn->query($sql) === TRUE) {
+    $userId = $conn->insert_id;
+    $usersql = "select * from family_user where id=".$userId;
+    $result = $conn->query($usersql);
+    $data = $result->fetch_assoc();
+    $fileName = explode("assets/users/", $data['profileImage']);
+    mkdir("assets/users/" . $userId, 0700);
+    rename($data['profileImage'], $location = "assets/users/" . $userId . "/" . $fileName[1]);
+    $newLocation = "assets/users/" . $userId . "/" . $fileName[1];
+    $updatesql = "update family_user set profileImage='".$newLocation."' where id=".$userId;
+    $conn->query($updatesql);
+}
+header("Location: home.php");
+?>
