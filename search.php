94260de76fb7cf10d9349c0b1c09eb0e330dc69c
futuredev <futuredev1001@yandex.com>
2020-12-24 08:09:56 -0300

    Search people to add the relationship

---
+++ search.php
@@ -0,0 +1,31 @@
+<?php
+
+$firstname = $_POST['firstName'];
+$middlename = $_POST['middleName'];
+$lastname = $_POST['lastName'];
+//$birthday = $_POST['birthday'];
+
+$servername = "database-1-instance-1.cwsml3emw73t.us-east-2.rds.amazonaws.com";
+$db_username = 'admin';
+$db_password = 'bit7wise';
+//$servername = "localhost";
+//$db_username = 'root';
+//$db_password = '';
+
+$databasename = "memoreebook";
+$conn = new mysqli($servername, $db_username, $db_password, $databasename);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select * from family_user where firstName like'%" . $firstname . "%' and lastName like '%" . $lastname . "%' and middleName like '%" . $middlename . "%'";
+$conn->query($sql);
+$data = [];
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        array_push($data, $row);
+    }
+}
+echo json_encode($data);
+
